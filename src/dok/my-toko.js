2d1c9c6f9217376d1b7626478bfced58a158ab8d
Creativitas <112189857+creativitas@users.noreply.github.com>
2024-06-16 13:17:08 +0700

    Update my-toko.js

--- src/dok/my-toko.js
+++ src/dok/my-toko.js
@@ -21,42 +21,42 @@ export class MyToko extends LitElement {
         {
           nav: 'BLACKS',
           image:
-            'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj5eMTzo-d0A4p6o9OH979U7Coqu8Io1AuSsyZQo-DRahqhyphenhyphenx06euPLnFI03eSPZ89fjpkCIpuHM4f2G2Oa3EYVfJO-JAH0lvy3PR9WVdR7e1GFCBxUADj-4aCxGqSl91Puw8eISX8gqZMpgvNStjLAlMx-HVZIXxPFI8BQ0AuOtHlx8HK9A9AzYsLqm2M0/s16000/photographer%20gallery%20website%20image.jpg',
+            'https://wsrv.nl/?url=https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj5eMTzo-d0A4p6o9OH979U7Coqu8Io1AuSsyZQo-DRahqhyphenhyphenx06euPLnFI03eSPZ89fjpkCIpuHM4f2G2Oa3EYVfJO-JAH0lvy3PR9WVdR7e1GFCBxUADj-4aCxGqSl91Puw8eISX8gqZMpgvNStjLAlMx-HVZIXxPFI8BQ0AuOtHlx8HK9A9AzYsLqm2M0/s16000/photographer%20gallery%20website%20image.jpg',
           info: 'Multipurpose Astro JS themes template',
           link: 'https://creativitas.dev/theme/astro-multipurpose/',
         },
         {
           nav: 'TEXT-X',
           image:
-            'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgHZIphI-1rYH7jIIdyT88c_SPNipzIZfIZefwL7mLr1jMUMJEbEhQ9aEWIcWGtKPU1RZ4POC1ma2e3qTbrN_egIeHKjU9sAi5t5ol6I5ZlqjMjZ5PadjAQ0fHnF0JOCQbZyF1DxniprL99ssdgPXoTzLeMo1fETHg2KTiS1gcALp7FHgrxcun_KyYBe-NO/s16000/minimalist-blog-text-x.webpp',
+            'https://wsrv.nl/?url=https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgHZIphI-1rYH7jIIdyT88c_SPNipzIZfIZefwL7mLr1jMUMJEbEhQ9aEWIcWGtKPU1RZ4POC1ma2e3qTbrN_egIeHKjU9sAi5t5ol6I5ZlqjMjZ5PadjAQ0fHnF0JOCQbZyF1DxniprL99ssdgPXoTzLeMo1fETHg2KTiS1gcALp7FHgrxcun_KyYBe-NO/s16000/minimalist-blog-text-x.webpp',
           info: 'Minimalist Clean Flatfile CMS themes template',
           link: 'https://creativitas.dev/theme/minimalist-clean-website-themes/',
         },
         {
           nav: 'DARKCO',
           image:
-            'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi1S5suUlq2S0-K6ZKyIYX0ndWC7cs1I1qR_ZmwHqa7v9K7-ihG3ar0uU9irUeSpopO6atKgBY32Q3L5PgT7Bv_zFPtemvZ1j85enNTpM5T8r96DPSt-Vhe4dJS3l_jULFH3WyVG-kg0tA9zdLytjEvkaF7-OvgD5RgnwxP3heetrdSpGTfgDnFq2OGoS7w/s16000/dark-website-attractive-themes.webp',
+            'https://wsrv.nl/?url=https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi1S5suUlq2S0-K6ZKyIYX0ndWC7cs1I1qR_ZmwHqa7v9K7-ihG3ar0uU9irUeSpopO6atKgBY32Q3L5PgT7Bv_zFPtemvZ1j85enNTpM5T8r96DPSt-Vhe4dJS3l_jULFH3WyVG-kg0tA9zdLytjEvkaF7-OvgD5RgnwxP3heetrdSpGTfgDnFq2OGoS7w/s16000/dark-website-attractive-themes.webp',
           info: 'New dark mode version flatfile CMS',
           link: 'https://creativitas.dev/theme/dark-new-modern-trend-website/',
         },
         {
           nav: 'RENT CAR',
           image:
-            'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEghpDqhfbD3ZR75EBcZqguS53Kq5Vh1-Z3L48-sD8TtYp23h2jMPskO8b743fQg1wuBDG8yVZJEoFNg3Y4yitT5jVMKzpdzCr-Bu2kz3gWjSN7ny7rXWg6wtTRToSe9h_vAaSgx1d2HyS2oiYLXYCfME8pozcFB2EQ2y3w1yerblV5IYi-xOXKEzoZyqSlv/s16000/astro-rent-a-car.jpg',
+            'https://wsrv.nl/?url=https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEghpDqhfbD3ZR75EBcZqguS53Kq5Vh1-Z3L48-sD8TtYp23h2jMPskO8b743fQg1wuBDG8yVZJEoFNg3Y4yitT5jVMKzpdzCr-Bu2kz3gWjSN7ny7rXWg6wtTRToSe9h_vAaSgx1d2HyS2oiYLXYCfME8pozcFB2EQ2y3w1yerblV5IYi-xOXKEzoZyqSlv/s16000/astro-rent-a-car.jpg',
           info: 'Built your modern rent car website with astro js + book form',
           link: 'https://creativitas.dev/theme/astro-rent-car/',
         },
         {
           nav: 'TAILWIND BLOG',
           image:
-            'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhu-qQcnPeDbjE_V7I7B1u3wZSuBN8DJC_9rIu3oV36r90-7yMhzfjvDhsd76QSsup05ydxjcFhhSXYgR2EBGTwnKMkjWz9am4KVus_yO5mZKGdRyQwrX-h7uu7DaEVVLZGGbvQCvzESq37QOB8qHbX8VG1R2H0v6dl417uAymQskeB1WCxvsVgGJJ_bR00/s16000/tailwindcms.webp',
+            'https://wsrv.nl/?url=https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhu-qQcnPeDbjE_V7I7B1u3wZSuBN8DJC_9rIu3oV36r90-7yMhzfjvDhsd76QSsup05ydxjcFhhSXYgR2EBGTwnKMkjWz9am4KVus_yO5mZKGdRyQwrX-h7uu7DaEVVLZGGbvQCvzESq37QOB8qHbX8VG1R2H0v6dl417uAymQskeB1WCxvsVgGJJ_bR00/s16000/tailwindcms.webp',
           info: 'Lets build your project with tailwind blog + backend admin flatfile cms',
           link: 'https://creativitas.dev/theme/tailwind-blog/',
         },
         {
           nav: 'MOBILE WEB',
           image:
-            'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjXAt-H9GXV6hAqIZ5xsQhyphenhyphenRRngEeZsg03tnTTXnmifBc57MZ8c3_sHgzSkC-Fk7YV8NCLf0H8RoHKbeBceGY1JouG7pngyRP6nBKKGOX376zum0UnrMZaFD9OiW1RKn6afnOzz1H_fBHlNyTxXcDrBb4wL13lKnGMa3FhNVUsrygDvLHrIFFLEISJotyXc/s16000/mobileweb-markdown.png',
+            'https://wsrv.nl/?url=https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjXAt-H9GXV6hAqIZ5xsQhyphenhyphenRRngEeZsg03tnTTXnmifBc57MZ8c3_sHgzSkC-Fk7YV8NCLf0H8RoHKbeBceGY1JouG7pngyRP6nBKKGOX376zum0UnrMZaFD9OiW1RKn6afnOzz1H_fBHlNyTxXcDrBb4wL13lKnGMa3FhNVUsrygDvLHrIFFLEISJotyXc/s16000/mobileweb-markdown.png',
           info: 'Get start now built mobile web with markdown PHP',
           link: 'https://creativitas.dev/theme/mobile-web-markdown-php/',
         },
